d77cafec9d34fcf0ca09dbfe6bd90e534a5fd2ea
Dawid Segieta <buler43221@gmail.com>
2023-01-29 13:57:23 +0100

    addClassAmountWidget

--- src/js/script.js
+++ src/js/script.js
@@ -43,8 +43,8 @@
   const settings = {
     amountWidget: {
       defaultValue: 1,
-      defaultMin: 1,
-      defaultMax: 9,
+      defaultMin: 0,
+      defaultMax: 10,
     }
   };
 
@@ -133,55 +133,39 @@
     processOrder(){
       const thisProduct = this;
       //console.log('processOrder:', thisProduct);
-
       // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
       //console.log('formData:', formData);
-
       // set price to default price
       let price = thisProduct.data.price;
-
       // for every category (param)...
       for(let paramId in thisProduct.data.params) {
-
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-
         //console.log(paramId, param);
         // for every option in this category
         for(let optionId in param.options) {
-
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-
           // check if there is param with a name of paramId in formData and if it includes optionId
           const optionSelected =  formData[paramId] && formData[paramId].includes(optionId); 
-          
           // check if the option is not default
           if (optionSelected) {
-
             // add option price to price variable
             price += option.price;
-            
           } else {
-
             // check if the option is default
             if (optionSelected) {
-
               // reduce price variable
               price -= option.price;
             }
           }
-        
           //find image with class paramId.optionId
           const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
-
           //check if optionImage is found
           if(optionImage) {
-
             //if optionImage is found check if option is selected
             if(optionSelected) {
-
               //if option is selected or not, add or remove class active
               optionImage.classList.add(classNames.menuProduct.imageVisible);
             } else {
@@ -189,8 +173,9 @@
             }
           }
         }
-      }
-      
+      } 
+      /* multiply price by amount*/
+      price *= thisProduct.amountWidget.value;
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
     }
@@ -198,6 +183,10 @@
     initAmountWidget(){
       const thisProduct = this;
       thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+
+      thisProduct.amountWidgetElem.addEventListener('updated', function () {
+        thisProduct.processOrder();
+      });
     }
   }
 
@@ -208,6 +197,8 @@
       console.log('cosntructor arguments:', element);
 
       thisWidget.getElements(element);
+      thisWidget.setValue(settings.amountWidget.defaultValue);
+      thisWidget.initActions();
     }
   
     getElements(element){
@@ -222,12 +213,44 @@
     setValue(value){
       const thisWidget = this;
       const newValue = parseInt(value);
-      //TODO: add validation
 
-      thisWidget.value = newValue;
+      /* TODO: Add validation */
+      if (
+        thisWidget.value !== newValue &&
+        !isNaN(newValue) &&
+        settings.amountWidget.defaultMin <= newValue &&
+        settings.amountWidget.defaultMax >= newValue
+      ) {
+        thisWidget.value = newValue;
+      }
+
       thisWidget.input.value = thisWidget.value;
+      thisWidget.announce();
     }
 
+    initActions(){
+      const thisWidget = this;
+      thisWidget.input.addEventListener('change', function(){
+        thisWidget.setValue(thisWidget.input.value);
+      });
+
+      thisWidget.linkDecrease.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value - 1);
+      });
+
+      thisWidget.linkIncrease.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value + 1);
+      });
+    }
+
+    announce(){
+      const thisWidget = this;
+
+      const event = new Event('updated');
+      thisWidget.element.dispatchEvent(event);
+    }
   }
 
   const app = {
